1bc81b593f0a303dcdf76a4de97673c3285d1f0e
Matheus Alexandre <matheusalexmc@gmail.com>
2022-10-27 16:44:44 -0300

    Calcular o valor de PI

---
+++ Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PI
+{
+    class Calculadora
+    {
+        public static double pi = 3.14; // static para ser chamado sem instanciar o objeto
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * pi * r*r*r;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace PI
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com o valor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
